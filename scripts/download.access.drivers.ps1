147a9c126be3a5e18d1d0bd26c0546114dafc730
Davi Mulek <david.mulek@volvo.com>
2021-05-15 10:50:01 -0300

    Fixed script MSI extraction.

--- scripts/download.access.drivers.ps1
+++ scripts/download.access.drivers.ps1
@@ -2,6 +2,11 @@
     "https://download.microsoft.com/download/2/4/3/24375141-E08D-4803-AB0E-10F2E3A07AAA/AccessDatabaseEngine.exe",
     "https://download.microsoft.com/download/2/4/3/24375141-E08D-4803-AB0E-10F2E3A07AAA/AccessDatabaseEngine_X64.exe"
 )
+[System.IO.DirectoryInfo]$tempFolder = Join-Path $PSScriptRoot "temp"
+if ($tempFolder.Exists) {
+    Remove-Item -Recurse -Force $tempFolder
+}
+. { mkdir $tempFolder } | Out-Null
 
 foreach ($link in $links) {
     [System.IO.FileInfo]$fileInfo = Join-Path $PSScriptRoot ($link.Segments | Select-Object -Last 1)
@@ -16,18 +21,14 @@ foreach ($link in $links) {
     }
 
     [string]$id = [system.IO.Path]::GetFileNameWithoutExtension($fileInfo.Name)
-    [System.IO.DirectoryInfo]$tempFolder = Join-Path $PSScriptRoot $id
-    if ($tempFolder.Exists) {
-        . { Remove-Item -Force -Recurse $tempFolder } | Out-Null
-    }
+    [System.IO.DirectoryInfo]$exeTempFolder = Join-Path $tempFolder $id "exe"
+    . { mkdir $exeTempFolder } | Out-Null
 
-    . { mkdir $tempFolder } | Out-Null
+    [System.IO.FileInfo]$exelogFile = Join-Path $exeTempFolder "$id.log"
 
-    [System.IO.FileInfo]$logFile = Join-Path $tempFolder "$id.log"
-
-    Start-Process -Wait -FilePath "$($fileInfo.FullName)" -ArgumentList "/extract:$($tempFolder.FullName)", "/log:$($logFile.FullName)", "/quiet", "/passive", "/norestart"
+    Start-Process -Wait -FilePath "$($fileInfo.FullName)" -ArgumentList "/extract:$($exeTempFolder.FullName)", "/log:$($exelogFile.FullName)", "/quiet", "/passive", "/norestart"
     
-    [System.IO.FileInfo]$aceRedistFileInfo = Join-Path $tempFolder "AceRedist.msi"
+    [System.IO.FileInfo]$aceRedistFileInfo = Join-Path $exeTempFolder "AceRedist.msi"
     Write-Host $aceRedistFileInfo
     if (-not $aceRedistFileInfo.Exists) {
         [string]$errorMessage = "❌  Extraction has failed..."
@@ -35,5 +36,9 @@ foreach ($link in $links) {
         throw $errorMessage
     }
 
-    Start-Process -Wait msiexec -ArgumentList "/a", "$($aceRedistFileInfo.FullName)", "/qb", "TARGETDIR=`"$($tempFolder.FullName)`"", "/l*v", "$($logFile.FullName)"
+    [System.IO.DirectoryInfo]$msiTempFolder = Join-Path $tempFolder $id "msi"
+    . { mkdir $msiTempFolder } | Out-Null
+    [System.IO.FileInfo]$msilogFile = Join-Path $msiTempFolder "$id.log"
+
+    Start-Process -Wait msiexec -ArgumentList "/a", "$($aceRedistFileInfo.FullName)", "TARGETDIR=`"$($msiTempFolder.FullName)`"", "/l*v", "$($msilogFile.FullName)"
 }
